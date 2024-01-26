ec41775d20a915e2c7f954979bab3658b716c7e3
imeldaidk <mellyabudei@gmail.com>
2024-01-26 14:03:20 -0800

    task 8

---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,8 @@
+'def add_integer(a, b=98):'
+	'def matrix_divided(matrix, div):'
+	'def say_my_name(first_name, last_name=""):'
+	'def print_square(size):'
+		'def text_indentation(text):'
+			'def matrix_mul(m_a, m_b):'
+				'def lazy_matrix_mul(m_a, m_b):'
+					'void print_python_string(PyObject *p);'
